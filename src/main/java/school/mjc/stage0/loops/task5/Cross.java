4966bc5e17036e0b3a8947540fd503dcf372f69b
tabletka97 <mtabletka.by@gmail.com>
2023-02-02 16:33:42 +0300

    task5 done

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                System.out.print(i == Math.round((double) sideLength / 2) ? "8"
+                        : j == Math.round((double) sideLength / 2) ? "8" : " "
+                );
+            }
+            System.out.println();
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,37 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for (int i = 1; i <= height; i++) {
+            if (i <= height / 2) {
+                for (int j = 0; j < i - 1; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < height - (i - 1) * 2; j++) {
+                    System.out.print(8);
+                }
+                for (int j = 0; j < i - 1; j++) {
+                    System.out.print(" ");
+                }
+            } else {
+                int spaces = 0;
+                for (int j = height - i; j > 0; j--) {
+                    System.out.print(" ");
+                    spaces++;
+                }
+                for (int j = height - spaces * 2; j > 0; j--) {
+                    System.out.print(8);
+                }for (int j = height - i; j > 0; j--) {
+                    System.out.print(" ");
+                }
+            }
 
+            System.out.println();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Hourglass().printHourglassOfGivenSize(7);
     }
 }
